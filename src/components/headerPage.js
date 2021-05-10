f68b85f76f8f382981431f4d0e3801f0177c0347
Jorge <jsilva@bombano.com>
2021-05-10 09:35:56 -0400

    Fix slug errors (feedCarousel) and sectionRendering

--- src/components/headerPage.js
+++ src/components/headerPage.js
@@ -5,7 +5,7 @@ import { Helmet } from "react-helmet"
 // Components
 import SEO from '../components/seo/seoGatsby'
 
-export default function HeaderPage( { title, location, cover, description, article } ){
+export default function HeaderPage( { title, location, cover, description, article, className } ){
 
     const url = location.href ? location.href : ''
     const current = location.pathname.split( '/' )
@@ -13,7 +13,7 @@ export default function HeaderPage( { title, location, cover, description, artic
     return (
         <>
             <Helmet>
-                <html class={`page-${current[1]}${ (current[2]) ? ' ' + current[2] : ''}`} />
+                <html class={`page-${current[1]}${ (current[2]) ? ' ' + current[2] : ''} ${ (className) ? className : '' }`} />
             </Helmet>
             <SEO 
                 postPath={url} 
